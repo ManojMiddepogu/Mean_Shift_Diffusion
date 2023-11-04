a0cc17a59e510e2be21018ce18ea8e09fdaf753c
Chanukya Vardhan Gujjula <crg9968@log-3.hpc.nyu.edu>
2023-11-04 16:41:23 -0400

    fix initial files

--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -2,12 +2,10 @@ import argparse
 import inspect
 
 from . import gaussian_diffusion as gd
-from . import guided_gaussian_diffusion as ggd
 from .respace import SpacedDiffusion, space_timesteps
 from .unet import SuperResModel, UNetModel, EncoderUNetModel
 
-# NUM_CLASSES = 1000
-NUM_CLASSES = 10
+NUM_CLASSES = 1000
 
 
 def diffusion_defaults():
@@ -66,30 +64,6 @@ def model_and_diffusion_defaults():
     res.update(diffusion_defaults())
     return res
 
-def guided_model_and_diffusion_defaults():
-    """
-    Defaults for image training.
-    """
-    res = dict(
-        image_size=64,
-        num_channels=128,
-        num_res_blocks=2,
-        num_heads=4,
-        num_heads_upsample=-1,
-        num_head_channels=-1,
-        attention_resolutions="16,8",
-        channel_mult="",
-        dropout=0.0,
-        class_cond=False,
-        use_checkpoint=False,
-        use_scale_shift_norm=True,
-        resblock_updown=False,
-        use_fp16=False,
-        use_new_attention_order=False,
-    )
-    res.update(diffusion_defaults())
-    return res
-
 
 def classifier_and_diffusion_defaults():
     res = classifier_defaults()
@@ -123,85 +97,6 @@ def create_model_and_diffusion(
     use_new_attention_order,
 ):
     model = create_model(
-        image_size,
-        num_channels,
-        num_res_blocks,
-        channel_mult=channel_mult,
-        learn_sigma=learn_sigma,
-        class_cond=class_cond,
-        use_checkpoint=use_checkpoint,
-        attention_resolutions=attention_resolutions,
-        num_heads=num_heads,
-        num_head_channels=num_head_channels,
-        num_heads_upsample=num_heads_upsample,
-        use_scale_shift_norm=use_scale_shift_norm,
-        dropout=dropout,
-        resblock_updown=resblock_updown,
-        use_fp16=use_fp16,
-        use_new_attention_order=use_new_attention_order,
-    )
-    diffusion = create_gaussian_diffusion(
-        steps=diffusion_steps,
-        learn_sigma=learn_sigma,
-        noise_schedule=noise_schedule,
-        use_kl=use_kl,
-        predict_xstart=predict_xstart,
-        rescale_timesteps=rescale_timesteps,
-        rescale_learned_sigmas=rescale_learned_sigmas,
-        timestep_respacing=timestep_respacing,
-    )
-    return model, diffusion
-
-def create_guided_models_and_diffusion(
-    image_size,
-    class_cond,
-    learn_sigma,
-    num_channels,
-    num_res_blocks,
-    channel_mult,
-    num_heads,
-    num_head_channels,
-    num_heads_upsample,
-    attention_resolutions,
-    dropout,
-    diffusion_steps,
-    noise_schedule,
-    timestep_respacing,
-    use_kl,
-    predict_xstart,
-    rescale_timesteps,
-    rescale_learned_sigmas,
-    use_checkpoint,
-    use_scale_shift_norm,
-    resblock_updown,
-    use_fp16,
-    use_new_attention_order,
-):
-    # CHECK - NEED TO FIX CHANNEL MULTIPLIER IN CREATE MODEL
-    guidance_model = create_guidance_model(
-        image_size,
-        num_channels,
-        num_classes=NUM_CLASSES
-    )
-    guidance_model = create_model(
-        image_size,
-        num_channels, 
-        num_res_blocks,
-        channel_mult=channel_mult,
-        learn_sigma=learn_sigma, # CHECK - THIS PARAMETER GIVES 6 OUTPUT CHANNELS, CAN WE DIRECTLY USE THIS INSTEAD OF TWO NETWORKS??
-        class_cond=class_cond, # CHECK - NEED TO USE THIS PARAMETER, BUT NOT IMPACT THE UNET. IF THIS IS TRUE THEN ONLY Y IS USED IN THE EMBEDDING OF THE MODEL. IS IT BETTER TO CREATE A SEPARATE PARAMETER THAT SAYS USE GUIDANCE AND LINK IT TO CLASS_COND CASES IN CREATE_MODEL() OR HAVE A SEPARETE MODEL ITSELF??
-        use_checkpoint=use_checkpoint,
-        attention_resolutions=attention_resolutions,
-        num_heads=num_heads,
-        num_head_channels=num_head_channels,
-        num_heads_upsample=num_heads_upsample,
-        use_scale_shift_norm=use_scale_shift_norm,
-        dropout=dropout,
-        resblock_updown=resblock_updown,
-        use_fp16=use_fp16,
-        use_new_attention_order=use_new_attention_order,
-    )
-    denoise_model = create_model(
         image_size,
         num_channels,
         num_res_blocks,
@@ -528,55 +423,6 @@ def create_gaussian_diffusion(
         rescale_timesteps=rescale_timesteps,
     )
 
-def create_guided_gaussian_diffusion(
-    *,
-    steps=1000,
-    sigma_small=False,
-    noise_schedule="linear",
-    guidance_loss_type="kl",
-    denoise_loss_type="mse",
-    rescale_timesteps=False,
-    timestep_respacing="",
-):
-    # CHECK - FIX EVERYTHING BASED ON GUIDED_GAUSSIAN_DIFFUSION
-    betas = ggd.get_named_beta_schedule(noise_schedule, steps)
-    # guidance loss type
-    if guidance_loss_type == "kl":
-        guidance_loss_type = ggd.GuidanceLossType.KL
-    else if guidance_loss_type == "js":
-        guidance_loss_type = ggd.GuidanceLossType.JS
-    else:
-        raise NotImplementedError(guidance_loss_type)
-
-    # denoising loss type
-    if denoise_loss_type == "mse":
-        denoise_loss_type = ggd.DenoiseLossType.MSE
-    else:
-        raise NotImplementedError(denoise_loss_type)
-
-    if not timestep_respacing:
-        timestep_respacing = [steps]
-
-    # CHECK - FIX THIS COMPLETELY
-    return GuidedSpacedDiffusion(
-        use_timesteps=space_timesteps(steps, timestep_respacing),
-        betas=betas,
-        model_mean_type=(
-            gd.ModelMeanType.EPSILON if not predict_xstart else gd.ModelMeanType.START_X
-        ),
-        model_var_type=(
-            (
-                gd.ModelVarType.FIXED_LARGE
-                if not sigma_small
-                else gd.ModelVarType.FIXED_SMALL
-            )
-            if not learn_sigma
-            else gd.ModelVarType.LEARNED_RANGE
-        ),
-        guidance_loss_type=guidance_loss_type,
-        denoise_loss_type=denoise_loss_type,
-        rescale_timesteps=rescale_timesteps,
-    )
 
 def add_dict_to_argparser(parser, default_dict):
     for k, v in default_dict.items():
