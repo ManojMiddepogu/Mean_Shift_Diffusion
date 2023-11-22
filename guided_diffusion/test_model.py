5236222c1d7e5f333e33e325c58d4e77b1fa9353
Chanukya Vardhan Gujjula <crg9968@log-3.hpc.nyu.edu>
2023-11-22 05:45:22 -0500

    add test_model

---
+++ guided_diffusion/test_model.py
@@ -0,0 +1,86 @@
+from abc import abstractmethod
+
+import math
+
+import numpy as np
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .fp16_util import convert_module_to_f16, convert_module_to_f32
+from .nn import (
+    checkpoint,
+    conv_nd,
+    linear,
+    avg_pool_nd,
+    zero_module,
+    normalization,
+    timestep_embedding,
+)
+from .unet import UNetModel
+
+import os
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
+class GuidanceModel(nn.Module):
+
+    def __init__(
+        self
+    ):
+        super().__init__()
+    
+    def forward(self, y, t):
+        b = y['y'].shape[0]
+        # return th.randn(b, 3, 32, 32).to('cuda'), th.randn(b, 1).to('cuda')
+        return th.zeros(b, 3, 32, 32).to('cuda').detach(), th.ones(b, 1).to('cuda').detach()
+
+class TestModel(nn.Module):
+
+    def __init__(
+        self,
+        image_size,
+        in_channels,
+        model_channels,
+        out_channels,
+        num_res_blocks,
+        attention_resolutions,
+        dropout=0,
+        channel_mult=(1, 2, 4, 8),
+        conv_resample=True,
+        dims=2,
+        num_classes=None,
+        use_checkpoint=False,
+        use_fp16=False,
+        num_heads=1,
+        num_head_channels=-1,
+        num_heads_upsample=-1,
+        use_scale_shift_norm=False,
+        resblock_updown=False,
+        use_new_attention_order=False,
+    ):
+        super().__init__()
+
+        self.guidance_model = GuidanceModel()
+
+        self.denoise_model = UNetModel(
+            image_size=image_size,
+            in_channels=in_channels,
+            model_channels=model_channels,
+            out_channels=out_channels,
+            num_res_blocks=num_res_blocks,
+            attention_resolutions=attention_resolutions,
+            dropout=dropout,
+            channel_mult=channel_mult,
+            num_classes=num_classes,
+            use_checkpoint=use_checkpoint,
+            use_fp16=use_fp16,
+            num_heads=num_heads,
+            num_head_channels=num_head_channels,
+            num_heads_upsample=num_heads_upsample,
+            use_scale_shift_norm=use_scale_shift_norm,
+            resblock_updown=resblock_updown,
+            use_new_attention_order=use_new_attention_order,
+        )
+    
+    def forward(self):
+        raise NotImplementedError(f"SHOULD NOT BE CALLED!")
