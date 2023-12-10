78d06fcb5cdc484c6f7501fa14d65f158bded418
Chanukya Vardhan Gujjula <crg9968@log-2.hpc.nyu.edu>
2023-12-10 01:12:20 -0500

    Add run experiments script

---
+++ run_experiments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+data_dir="/scratch/crg9968/datasets/cifar10"
+training_data_inception_mu_sigma_path="/scratch/crg9968/datasets/cifar10/cifar_train_mu_sigma.npz"
+distance=0.1
+no_guidance_step=4000
+lr_anneal_steps=50001
+save_interval=1000
+fid_interval=2000
+num_samples=1000
+num_samples_batch_size=500
+
+# Baseline
+sbatch ./baseline_cifar_job.slurm "/scratch/crg9968/llvm/logs_0" $data_dir $training_data_inception_mu_sigma_path \
+    "uniform" $lr_anneal_steps $save_interval $fid_interval $num_samples $num_samples_batch_size
+
+# Clustered, Alternate, Freeze
+sbatch ./cifar_clustered_train_job.slurm "/scratch/crg9968/llvm/logs_1" $data_dir $training_data_inception_mu_sigma_path $distance \
+    "alternate" $no_guidance_step "True" $lr_anneal_steps $save_interval $fid_interval $num_samples $num_samples_batch_size
+
+# Clustered, Alternate, No Freeze
+sbatch ./cifar_clustered_train_job.slurm "/scratch/crg9968/llvm/logs_2" $data_dir $training_data_inception_mu_sigma_path $distance \
+    "alternate" $no_guidance_step "False" $lr_anneal_steps $save_interval $fid_interval $num_samples $num_samples_batch_size
+
+# Clustered, GANType, Freeze
+sbatch ./cifar_clustered_train_job.slurm "/scratch/crg9968/llvm/logs_3" $data_dir $training_data_inception_mu_sigma_path $distance \
+    "gantype" $no_guidance_step "True" $lr_anneal_steps $save_interval $fid_interval $num_samples $num_samples_batch_size
+
+# Clustered, GANType, No Freeze
+sbatch ./cifar_clustered_train_job.slurm "/scratch/crg9968/llvm/logs_4" $data_dir $training_data_inception_mu_sigma_path $distance \
+    "gantype" $no_guidance_step "False" $lr_anneal_steps $save_interval $fid_interval $num_samples $num_samples_batch_size
